438df998633828b1e05854b4d490e924fa6d7b04
qiuqiu <1403336200@qq.com>
2026-09-11 16:37:38 +0800

    调整常温手工录入数据的产品名称

--- src/views/LogINveic/NomalData-add-updata.vue
+++ src/views/LogINveic/NomalData-add-updata.vue
@@ -122,9 +122,9 @@ export default {
                 areaName: '四川',
                 deliverydate: '',//到货周期
                 vouchdate: '',//下单日期
-                factoryProductCode: '10500082',
-                factoryProductName: '光明噜渴原味酸奶饮品(常温)300ml-1*12纸箱(藏文版)',
-                productCode: '1520100062',
+                factoryProductCode: '10500128',
+                factoryProductName: '光明噜渴原味酸奶饮品(常温)300g-1*12纸箱(藏文版)',
+                productCode: '1520100080',
                 piece: '',
                 wlSiteName: 'LOOK雨帆自提',
                 wlSiteCode: '12117991',
